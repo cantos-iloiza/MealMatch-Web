5abc6bc680b9a32d4935472dccf9c01156319e49
francineannealvarez <23-09780@g.batstate-u.edu.ph>
2025-12-03 17:00:04 +0800

    firebase connection: done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Kreait\Firebase\Factory;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-firebase', function () {
+    try {
+        // Version 7.x syntax
+        $factory = (new Factory)->withServiceAccount(
+            storage_path('firebase/mealmatch-web-firebase-adminsdk-fbsvc-7cc8a28f53.json')
+        );
+        
+        $database = $factory->createDatabase();
+        
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Firebase connection working!',
+            'version' => '7.24.0'
+        ]);
+        
+    } catch (\Exception $e) {
+        return response()->json([
+            'status' => 'error',
+            'message' => $e->getMessage()
+        ]);
+    }
 });
